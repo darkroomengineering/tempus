da6365c5b0ad95ac156043bc0f1877f6e0fb2b9c
arzafran <franco@studiofreight.com>
2023-05-31 10:41:31 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -2,34 +2,59 @@ const isClient = typeof window !== 'undefined'
 
 class Tempus {
   constructor() {
-    this.callbacks = []
-    this.now = performance.now()
-    requestAnimationFrame(this.raf)
+    this.callbacks = new Map()
+    this.now = isClient ? performance.now() : 0
+    this.isDirty = false
+    if (isClient) {
+      requestAnimationFrame(this.raf)
+    } else {
+      console.warn('Tempus can be used in client side only')
+    }
   }
 
   add(callback, priority = 0) {
-    this.callbacks.push({ callback, priority })
-    this.callbacks.sort((a, b) => a.priority - b.priority)
+    if (!this.callbacks.has(priority)) {
+      this.callbacks.set(priority, new Set())
+    }
+    this.callbacks.get(priority).add(callback)
+    this.isDirty = true
 
-    return () => this.remove(callback)
+    return () => this.remove(callback, priority)
   }
 
-  remove(callback) {
-    this.callbacks = this.callbacks.filter(({ callback: cb }) => callback !== cb)
+  remove(callback, priority) {
+    if (this.callbacks.has(priority)) {
+      this.callbacks.get(priority).delete(callback)
+    }
   }
 
   raf = (now) => {
-    requestAnimationFrame(this.raf)
+    if (isClient) {
+      requestAnimationFrame(this.raf)
+    }
 
     const deltaTime = now - this.now
     this.now = now
 
-    for (let i = 0; i < this.callbacks.length; i++) {
-      this.callbacks[i].callback(now, deltaTime)
+    if (this.isDirty) {
+      this.sortCallbacks()
+      this.isDirty = false
     }
+
+    for (let [priority, callbacks] of this.callbacks) {
+      for (let callback of callbacks) {
+        try {
+          callback(now, deltaTime)
+        } catch (error) {
+          console.error(`Error in Tempus callback with priority ${priority}`, error)
+        }
+      }
+    }
+  }
+
+  sortCallbacks() {
+    this.callbacks = new Map([...this.callbacks.entries()].sort((a, b) => a[0] - b[0]))
   }
 }
 
-if (!isClient) console.warn('Tempus can be used in client side only')
-
-export default isClient && new Tempus()
+export default new Tempus()
